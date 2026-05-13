24a0462a1a6ebbe5fb2464663cdaf83d908eb3d1
Gulshan Jatwa <gulshan.jatwa@avantika.edu.in>
2026-05-13 20:04:55 +0530

    Improve variable names and scene setup
    Refactor scene handling and variable naming for clarity.

--- sketch.js
+++ sketch.js
@@ -1,37 +1,25 @@
-// 
-// UPI PULSE: THE FUTURE OF DIGITAL BHARAT 
-// Theme: India: Digital Payments (UPI) - Global Impact.
-
-let scene = 0;
-let particles = [];
-let ripples = [];
-let scanLineY = 0;
-let sceneAlpha = 255;
-let targetScene = 0;
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < 150; i++) {
-    particles.push(new Particle());
-  }
-}
-
-function draw() {
-  background(5, 8, 20); // Deep Space Blue
-  drawDigitalGrid();
-
-  // Background Particles
-  for (let p of particles) {
-    p.update();
-    p.show();
-  }
-
-  // Scene Logic with Smooth Fading
-  push();
-  if (scene == 0) introScene();
-  else if (scene == 1) indiaScene();
-  else if (scene == 2) globalScene();
-  else if (scene == 3) dashboardScene();
-  pop();
-
  
+// UPI PULSE: THE FUTURE OF DIGITAL BHARAT
+// India's Digital Payment System (the UPI) and itsInternational Importance.
+
+let currentScene = 0;
+let particles = [];
+let ripples =[];
+
+let scanLineY=0;let alphaTrans=255;
+
+let nextScene=0;let sceneAlphaPrev=1;let transitioning=false;
+
+let numberOfKeyInternationalColaborators=65;let numberOfActiveUsers=500+;
+
+let upiStepCount=0;
+let simScanY=0;let simLaserDirection=1;let authorizationTimer=0;
+
+let typedInAmount="";let typedInPin="";let currentTransactionID="";
+
+function setup()
+{
+  createCanvas(windowWidth,windowHeight);
+  for(let i=0;i<150;i++) particles.push(new Particle());
+  createSceneButtons();
+}
