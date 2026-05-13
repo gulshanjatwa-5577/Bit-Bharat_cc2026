f13b2c35dc9c768afff86e05e77982b036fb8018
Gulshan Jatwa <gulshan.jatwa@avantika.edu.in>
2026-05-13 20:07:42 +0530

    Added main draw loop and scene transitions

--- sketch.js
+++ sketch.js
@@ -23,3 +23,26 @@ function setup()
   for(let i=0;i<150;i++) particles.push(new Particle());
   createSceneButtons();
 }
+function draw() {
+  background(5, 8, 20);
+  drawDigitalGrid();
+  for (let p of particles) { p.update(); p.show(); }
+  for (let i = ripples.length - 1; i >= 0; i--) {
+    if (!ripples[i].update()) ripples.splice(i, 1);
+    else ripples[i].show();
+  }
+  if (isTransitioning) {
+    transitionProgress -= 0.05;
+    if (transitionProgress <= 0) {
+      scene = targetScene; isTransitioning = false; transitionProgress = 1;
+    }
+  }
+  let displayAlpha = isTransitioning ? map(transitionProgress, 1, 0, 255, 0) : 255;
+  push();
+  if (displayAlpha < 255) tint(255, displayAlpha);
+  if (scene == 0) introScene(); else if (scene == 1) simulatorScene(); 
+  else if (scene == 2) globalScene(); else if (scene == 3) dashboardScene();
+  pop();
+  scanLineY = (scanLineY + 2) % height;
+  stroke(0, 255, 200, 30); strokeWeight(1); line(0, scanLineY, width, scanLineY);
+}
